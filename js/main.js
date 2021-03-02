1f687bf7bff706730784821bba9b709f3732d8b7
HyCraftHD <hycrafthd@live.de>
2021-03-02 13:01:24 +0100

    Remove overlay stuff (old depcrecated stuff)

--- js/main.js
+++ js/main.js
@@ -1,9 +1,4 @@
 function loadMap(map) {
-    map = new UtilityMap("map", "de_inferno")
+    map = new UtilityMap("map", map)
     map.loadPoints()
-}
-
-function disableOverlay() {
-    //document.getElementById("overlay").style.display = "none";
-    document.querySelector("#overlay").style.display = "none";
 }
